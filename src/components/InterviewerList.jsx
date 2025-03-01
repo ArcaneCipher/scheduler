b5c0f1acc5aef2702d5b8579f628a246efd609f2
ArcaneCipher <desjardins.jarp@gmail.com>
2025-03-01 14:49:18 -0500

    Type Checking with Prop Types. Add prop-types library. Add PropTypes to InterviewerList

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'; 
 import "./InterviewerList.scss";
 
 import InterviewerListItem from "./InterviewerListItem";
@@ -23,5 +24,9 @@ function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 
 export default InterviewerList;
