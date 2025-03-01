249bca314e41d25abd4966dbdc85a5b964ca2598
ArcaneCipher <desjardins.jarp@gmail.com>
2025-02-28 23:58:42 -0500

    Build initial hook and test

---
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -0,0 +1,11 @@
+import { renderHook } from "@testing-library/react";
+
+import useVisualMode from "../useVisualMode";
+
+const FIRST = "FIRST";
+
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
